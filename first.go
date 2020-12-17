e5cef015c8ff20dbfe413dfce99c118dc2ed86cb
EmmCo <578844283@qq.com>
2020-12-17 20:53:40 +0800

    step by step

--- first.go
+++ first.go
@@ -2,14 +2,18 @@ package main
 
 import (
 	"database/sql"
+	"encoding/hex"
+	"encoding/json"
 	"fmt"
 	"github.com/garyburd/redigo/redis"
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 	"net/http"
+	"strings"
 	"time"
-
+    mgo "gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 	//"github.com/jmoiron/sqlx"
 )
 func gintest() {
@@ -111,11 +115,193 @@ func process(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, r.PostForm)
 	fmt.Fprintln(w, r.PostFormValue("hello"))
 }
-func main() {
+type DatabaseHandler interface {
+	AddEvent(Event) ([] byte, error)
+	FindEvent([]byte) (Event, error)
+	FindEventByName(string) (Event, error)
+	FindAllAvailableEvents()( []Event, error)
+}
 
+
+func (mgolayer *MongoDBLayer) getFreshSession() *mgo.Session {
+	return mgolayer.session.Copy()
+}
+
+func (mgolayer *MongoDBLayer) FindEvent(id []byte) (Event, error)  {
+	s := mgolayer.getFreshSession()
+	defer s.Close()
+	e := Event{}
+	err := s.DB(DB).C(EVENTS).FindId(bson.ObjectId(id)).One(&e)
+	return e, err
+}
+
+func (mgolayer *MongoDBLayer) FindAllAvailableEvents() ([]Event, error)  {
+	s := mgolayer.getFreshSession()
+	defer s.Close()
+	es := []Event{}
+	err := s.DB(DB).C(EVENTS).Find(nil ).All(&es)
+	return es, err
+}
+
+func (mgolayer *MongoDBLayer) FindEventByName(name string) (Event, error)  {
+	s := mgolayer.getFreshSession()
+	defer s.Close()
+	e := Event{}
+	err := s.DB(DB).C(EVENTS).Find(bson.M{"name": name }).One(&e)
+	return e, err
+}
+
+func (mgolayer *MongoDBLayer) AddEvent(e Event) ([]byte, error)  {
+	s := mgolayer.getFreshSession()
+	defer s.Close()
+	if !e.ID.Valid() {
+		e.ID = bson.NewObjectId()
+	}
+	if !e.Location.ID.Valid() {
+		e.Location.ID = bson.NewObjectId()
+	}
+	return []byte(e.ID), s.DB(DB).C(EVENTS).Insert(e)
+}
+
+
+type Event struct {
+	ID bson.ObjectId `bson:"_id"`
+	Name string
+	Duration int
+	StartDate int64
+	EndDate int64
+	Location Location
+}
+
+type Location struct {
+	ID bson.ObjectId `bson:"_id"`
+	Name string
+	Address string
+	Country string
+	OpenTime int
+	CloseTime int
+	Halls []Hall
+}
+
+type Hall struct {
+	Name string `json:"name"`
+	Location string `json:location,omitempty`
+	Capacity int `json:"capacity"`
+}
+
+type eventServiceHandler struct {
+	dbhandler DatabaseHandler
+}
+
+func newEventHandler(databasehandler DatabaseHandler) *eventServiceHandler {
+	return &eventServiceHandler {
+		dbhandler: databasehandler,
+	}
+}
+
+func (eh * eventServiceHandler) findEventHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	criteria, ok := vars["SearchCriteria"]
+	if !ok {
+		w.WriteHeader(400)
+		fmt.Fprint(w, `{error: No Search criteria found}`)
+		return
+	}
+	searchkey, ok := vars["Search"]
+	if !ok {
+		w.WriteHeader(400)
+		fmt.Fprint(w, `{error: No Search keys found}`)
+		return
+	}
+	var event Event
+	var err error
+	switch strings.ToLower(criteria) {
+	case "name":
+		event, err = eh.dbhandler.FindEventByName(searchkey)
+	case "id":
+		id, err := hex.DecodeString(searchkey)
+		if err == nil {
+			event, err = eh.dbhandler.FindEvent(id)
+		}
+	}
+	if err != nil {
+		w.WriteHeader(404)
+		fmt.Fprintf(w, "{error occured %s}", err)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json;charset=utf8")
+	json.NewEncoder(w).Encode(&event)
+}
+
+func (eh * eventServiceHandler) allEventHandler(w http.ResponseWriter, r *http.Request) {
+	events, err := eh.dbhandler.FindAllAvailableEvents()
+	if err != nil {
+		w.WriteHeader(500)
+		fmt.Fprintf(w, "{error: %s}", err)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json;charset=utf8")
+	json.NewEncoder(w).Encode(&events)
+}
+
+func (eh * eventServiceHandler) newEventHandler(w http.ResponseWriter, r *http.Request) {
+	event := Event{}
+	err := json.NewDecoder(r.Body).Decode(&event)
+	if err != nil {
+		w.WriteHeader(500)
+		fmt.Fprintf(w, "{error : %s}", err)
+		return
+	}
+	id, err := eh.dbhandler.AddEvent(event)
+	if nil != err {
+		w.WriteHeader(500)
+		fmt.Fprintf(w, "{id: %d, error: %s}", id, err)
+		return
+	}
+}
+
+const (
+	DB = "myevents"
+	USERS = "users"
+	EVENTS = "events"
+)
+type MongoDBLayer struct {
+	session *mgo.Session
+}
+
+func NewMongoDBLayer(connection string) (*MongoDBLayer, error) {
+	s, err := mgo.Dial(connection)
+	if err != nil {
+		return nil, err
+	}
+	return &MongoDBLayer {
+		session: s,
+	}, nil
+}
+
+func ServeAPI(endpoint string, dbHandler DatabaseHandler) error {
+	handler := newEventHandler(dbHandler)
 	rtr := mux.NewRouter()
 	rtr.HandleFunc("/pages/{id:[0-9]+}", pageHandler)
 	rtr.HandleFunc("/process", process)
+	eventsrouter := rtr.PathPrefix("/events").Subrouter()
+	eventsrouter.Methods("GET").
+		Path("/{SearchCriteria}/{search}").HandlerFunc(handler.findEventHandler)
+	eventsrouter.Methods("GET").Path("").HandlerFunc(handler.allEventHandler)
+	eventsrouter.Methods("POST").Path("").HandlerFunc(handler.allEventHandler)
+}
+
+func main() {
+	rtr := mux.NewRouter()
+	rtr.HandleFunc("/pages/{id:[0-9]+}", pageHandler)
+	rtr.HandleFunc("/process", process)
+	handler := &eventServiceHandler{}
+	eventsrouter := rtr.PathPrefix("/events").Subrouter()
+	eventsrouter.Methods("GET").
+		Path("/{SearchCriteria}/{search}").HandlerFunc(handler.findEventHandler)
+	eventsrouter.Methods("GET").Path("").HandlerFunc(handler.allEventHandler)
+	eventsrouter.Methods("POST").Path("").HandlerFunc(handler.allEventHandler)
+
 	//http.Handle("/", rtr)
 	http.ListenAndServe(PORT, rtr)
 	http.HandleFunc("/static",serveStatic)
--- go.mod
+++ go.mod
@@ -7,4 +7,5 @@ require (
 	github.com/gin-gonic/gin v1.6.3
 	github.com/go-sql-driver/mysql v1.5.0
 	github.com/gorilla/mux v1.8.0
+	gopkg.in/mgo.v2 v2.0.0-20190816093944-a6b53ec6cb22 // indirect
 )
