45696e79561ab5673a5fe12fdb4b9423147dfcf8
EmmCo <zhanghuaiyu_seu@163.com>
2020-12-19 11:44:59 +0800

    step by step

--- first.go
+++ first.go
@@ -7,6 +7,8 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"github.com/streadway/amqp"
+	"os"
 
 	//_ "github.com/go-sql-driver/mysql"
 	//"github.com/gorilla/mux"
@@ -16,6 +18,50 @@ import (
 )
 
 func main() {
+	amqpURL := os.Getenv("AMQP_URL")
+	if amqpURL == "" {
+		amqpURL = "amqp://guest:guest@localhost:5672"
+	}
+	connection, err := amqp.Dial(amqpURL)
+	if err != nil {
+		panic("could not establish AMQP connection: " + err.Error())
+	}
+	channel, err := connection.Channel()
+	if err != nil {
+		panic("could not open channel: " + err.Error())
+	}
+	channel.ExchangeDeclare("events", "topic",
+		true, false, false, false, nil)
+	message := amqp.Publishing{
+		Body : []byte("Hello world"),
+	}
+	err = channel.Publish("event", "some-routing-key",
+		false, false, message)
+	if err != nil {
+		panic("error while publishing message: " + err.Error())
+	}
+	_, err = channel.QueueDeclare("my_queue",
+		true, false, false, false, nil)
+	if err != nil {
+		panic("error while declaring the queue: " + err.Error())
+	}
+
+	err = channel.QueueBind("my_queue", "#", "events",
+		false, nil)
+	if err != nil {
+		panic("error while binding the queue: " + err.Error())
+	}
+
+	msgs, err := channel.Consume("my_queue", "",
+		false, false, false, false, nil)
+	if err != nil {
+		panic("error while consuming the queue: " + err.Error())
+	}
+	for msg := range msgs {
+		fmt.Println("message received: " + string(msg.Body))
+		msg.Ack(false)
+	}
+
 	confPath := flag.String("conf", `.\configuration\config.json`, "flag to set the path to the configuration json file")
 	flag.Parse()
 	//extract configuration
--- go.mod
+++ go.mod
@@ -7,5 +7,6 @@ require (
 	github.com/gin-gonic/gin v1.6.3
 	github.com/go-sql-driver/mysql v1.5.0
 	github.com/gorilla/mux v1.8.0
+	github.com/streadway/amqp v1.0.0 // indirect
 	gopkg.in/mgo.v2 v2.0.0-20190816093944-a6b53ec6cb22
 )
